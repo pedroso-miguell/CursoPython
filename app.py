ffbc44b009acc3e40ce707a8ccf159eb25463f57
pedroso-miguell <miguellarapedroso37@gmail.com>
2024-09-06 00:29:42 -0300

    Curso de Python finalizado!

---
+++ app.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/quemsomos')
+def quemsomos():
+    return render_template('quemsomos.html')
+
+@app.route('/localizacao')
+def localizacao():
+    return render_template('localizacao.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
